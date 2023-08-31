a60f9a5a6703635bd37b084f3909c872c96d68fe
Dominik <dominikbutz@gmail.com>
2023-08-31 18:45:55 +0200

    first deploy

---
+++ precache-manifest.1abf17c4e14b88cb208a1616019a888e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db639972611319d29373",
+    "url": "/githubPagesTest/static/css/main.afa9a20c.chunk.css"
+  },
+  {
+    "revision": "db639972611319d29373",
+    "url": "/githubPagesTest/static/js/main.db639972.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/githubPagesTest/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/githubPagesTest/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "c7510769ddcff7e35180",
+    "url": "/githubPagesTest/static/js/runtime~main.c7510769.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/githubPagesTest/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c2d48231faba942d72a13b22b3fa600b",
+    "url": "/githubPagesTest/index.html"
+  }
+];
